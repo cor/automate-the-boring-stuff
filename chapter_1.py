8c0bb520d7d2bfe2b81256f643a85670539c6a81
cor <cor@pruijs.nl>
2020-04-15 16:04:13 +0200

    Add Chapter 1

---
+++ chapter_1.py
@@ -0,0 +1,12 @@
+# Basic 'Hello world' type of program that asks for name and age
+print('Hello, world!')
+
+print('What is your name?')
+my_name = input()
+print(f'It is good to meet you, {my_name} ({len(my_name)})')
+
+print()
+
+print('What is your age?')
+my_age = input()
+print(f'You will be {str(int(my_age) + 1)} in a year.')
