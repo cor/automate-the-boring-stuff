eeb55afc56b9b00da7ed374aed72c00cbe842e6b
cor <cor@pruijs.nl>
2020-04-19 16:54:31 +0200

    Change coin flip streaks algorithm, refer to other implementations

--- chapter-4/coin_flip_streaks.py
+++ chapter-4/coin_flip_streaks.py
@@ -1,19 +1,25 @@
 import random
 from statistics import mean
 
+
+# For more implementations of this problem, including a test runner that check which one is the fastest, see:
+# https://github.com/cor/drudgery
+# for an implementation of this problem in Rust, see:
+# https://github.com/cor/coin-flip-streaks-rs
+
+
 # Generate a sequence of 100 'H' (Heads) and 'T' (Tails) throws
 # and return the amount of 6-long chains of the same value
 def experiment():
-    flip_results = random.choices(('H', 'T'), k=100)
+    flip_results = "".join(random.choices(('H', 'T'), k=100))
     streak_count = 0
-    for i in range(len(flip_results)-6):
-        if (flip_results[i] == 'H' and 'T' not in flip_results[i:i+6]) or \
-           (flip_results[i] == 'T' and 'H' not in flip_results[i:i+6]):
-            streak_count += 1
+    for i in range(len(flip_results) - 6):
+        streak_count += (flip_results[i:i + 6] == flip_results[i] * 6)
     return streak_count
 
+
 results = []
-for i in range(10_000):
+for _ in range(10_000):
     results.append(experiment())
 
 print(mean(results))
