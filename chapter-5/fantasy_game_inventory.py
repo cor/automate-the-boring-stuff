2b3ed922d1379dcc9768dbc98d3fe061955be999
cor <cor@pruijs.nl>
2020-04-20 15:43:29 +0200

    Add Fantasy Game Inventory

---
+++ chapter-5/fantasy_game_inventory.py
@@ -0,0 +1,20 @@
+my_inventory = {'gold coin': 42, 'rope': 1}
+
+
+def display_inventory(inventory):
+    """Display an inventory"""
+    for k, v in inventory.items():
+        print(f'{v} {k}')
+    print(f'Total number of items: {sum(inventory.values())}')
+
+
+def add_to_inventory(inventory, added_items):
+    """Add items to an inventory. Modifies the inventory parameter."""
+    for item in added_items:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+
+
+display_inventory(my_inventory)
+add_to_inventory(my_inventory, ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby'])
+display_inventory(my_inventory)
