b713ac75c2d4be4ae26f7b3a8bc7be36f8228c95
cor <cor@pruijs.nl>
2020-07-02 15:11:07 +0200

    Add is phone number

---
+++ chapter-7/is_phone_number.py
@@ -0,0 +1,7 @@
+import re
+
+phone_number_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+
+mo = phone_number_regex.search('My phone number is 415-555-4242')
+
+print(f'Phone number found: {mo.group()}')
