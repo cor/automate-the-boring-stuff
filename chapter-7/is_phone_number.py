4bbc7b1ba4173cc3855a3636acb345c6ee1e9808
cor <cor@pruijs.nl>
2020-07-02 15:26:02 +0200

    Use groups

--- chapter-7/is_phone_number.py
+++ chapter-7/is_phone_number.py
@@ -1,7 +1,12 @@
 import re
 
-phone_number_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phone_number_regex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 
 mo = phone_number_regex.search('My phone number is 415-555-4242')
 
 print(f'Phone number found: {mo.group()}')
+
+area_code, main_number = mo.groups()
+
+print(f'Area: {area_code}, Main number: {main_number}')
+
